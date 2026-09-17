eab4aa8cb70c472e82ca89ce9269abe8045b91b6
lucas <lucas@10.78.21.208>
2026-09-17 12:41:42 -0400

    Activity 1 Unit 2: Hello World

---
+++ ICS3U_Unit2/src/activity1/HelloWorld.java
@@ -0,0 +1,25 @@
+package activity1;
+
+import simpleIO.Console;
+
+/**@author Lucas Garner
+ * Date: September 17 2026
+ * Course: ICS3U
+ * HelloWorld.java
+ * A first 
+ * Java Program
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args command line argument
+	 */
+	public static void main(String[] args) {
+		// Output for user
+		
+		Console.print("Hello World!");
+
+	}
+
+}
